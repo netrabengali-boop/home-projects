c0e735995c6cdc47652367b2b2ab1cbd0c607016
netrabengali-boop <netrabengali@gmail.com>
2026-05-28 12:06:00 +0530

    Add files via upload

---
+++ python_callenges.py
@@ -0,0 +1,8 @@
+a = input("Enter first value (a): ")
+b = input("Enter second value (b): ")
+c = input("Enter third value (c): ")
+a, b, c = c, a, b
+print("\n After Swapping ")
+print(f"First value (a) : {a}")
+print(f"Second value (b) : {b}")
+print(f"Third value (c) : {c}")
